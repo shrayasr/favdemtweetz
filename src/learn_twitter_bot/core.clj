fe807ed2365952b2e8c750e27c16f4395eb26504
Shrayas <shrayasr@gmail.com>
2015-03-06 00:43:05 +0530

    Basic requirements of bot, done

--- src/learn_twitter_bot/core.clj
+++ src/learn_twitter_bot/core.clj
@@ -8,11 +8,15 @@
     (twitter.callbacks.protocols SyncSingleCallback))
   (:gen-class))
 
-(def my-creds (make-oauth-creds "7nRS6v8GqqWm3r7s4NsnCC7fX"
-                                "5ivF3wwvgPEbfB8FvJdl1c6X5tchJxsICd7hHDUiiryhVMIUN9"
-                                "46169732-tKiDKTaQNFBce04bGUuVku3QaNzdLNT6v3xTl2sNA"
-                                "DypaSi6CichUmjwSNMcsm2R8wyxkD675JkbYZDazUZ2SV"))
+(def my-creds (make-oauth-creds "phk6yVKreqqWYwkrKrV2LtXlN"
+                                "48eFKSQteSHuDRr2d00cvA5BPQLlaSRIBmN0TE3J8BZvgUtZrR"
+                                "3063370765-T7TlE9bfO6SEaV6dvvGV6mXltOYRnLP6sBvVrNM"
+                                "EmSeWB17z8qyPBKwbWoNsrUM7TDQciYTJga3ehAHWY7ba"))
 
 (defn -main
   []
-  (println (users-show :oauth-creds my-creds :params { :screen-name "shrayasr" })))
+  ;(println (lists-members :oauth-creds my-creds
+  ;            :params {:list_id "197805222"})))
+  (favorites-create :oauth-creds my-creds
+                     :params {:id "550993690659794945"})
+  (println "done"))
