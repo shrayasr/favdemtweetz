18f89b213c4471ef31e2d5e8bf6b5e8b301dfefb
Shrayas <shrayasr@gmail.com>
2015-03-05 22:13:39 +0530

    Print user information. Basic stuff from tutorial

--- src/learn_twitter_bot/core.clj
+++ src/learn_twitter_bot/core.clj
@@ -1,11 +1,18 @@
 (ns learn-twitter-bot.core
+  (:use
+    [twitter.oauth]
+    [twitter.callbacks]
+    [twitter.callbacks.handlers]
+    [twitter.api.restful])
+  (:import
+    (twitter.callbacks.protocols SyncSingleCallback))
   (:gen-class))
 
-(defn foo
-  "I don't do a whole lot."
-  []
-  (println "Hello, World!"))
+(def my-creds (make-oauth-creds "7nRS6v8GqqWm3r7s4NsnCC7fX"
+                                "5ivF3wwvgPEbfB8FvJdl1c6X5tchJxsICd7hHDUiiryhVMIUN9"
+                                "46169732-tKiDKTaQNFBce04bGUuVku3QaNzdLNT6v3xTl2sNA"
+                                "DypaSi6CichUmjwSNMcsm2R8wyxkD675JkbYZDazUZ2SV"))
 
 (defn -main
   []
-  (foo))
+  (println (users-show :oauth-creds my-creds :params { :screen-name "shrayasr" })))
