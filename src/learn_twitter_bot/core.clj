37220bf318ab7a7bca9b2f5e42ee10637441d8fc
Shrayas <shrayasr@gmail.com>
2015-03-07 00:58:11 +0530

    First working version of actual bot

--- src/learn_twitter_bot/core.clj
+++ src/learn_twitter_bot/core.clj
@@ -14,19 +14,36 @@
                                 "EmSeWB17z8qyPBKwbWoNsrUM7TDQciYTJga3ehAHWY7ba"))
 
 (def handles (atom ["raghothams" "argvk"]))
-(def handle-last-id (atom {"argvk" 1}))
+(def handle-last-id (atom {}))
 
 (defn get-last-id 
   [handle]
   (get @handle-last-id handle))
 
-(defn get-first-tweet-id-from-timeline 
+(defn get-most-recent-status-id
   [handle]
-  1)
+  (->
+    (statuses-user-timeline :oauth-creds my-creds
+                            :params {:screen-name handle
+                                     :exclude-replies true
+                                     :count 1
+                                     :include-rts false})
+    (get :body)
+    (first)
+    (get :id)))
 
-(defn get-tweet-id-since
-  [last-id]
-  2)
+(defn get-most-recent-status-id-since
+  [handle since-id]
+  (->
+    (statuses-user-timeline :oauth-creds my-creds
+                            :params {:screen-name handle
+                                     :exclude-replies true
+                                     :count 1
+                                     :since since-id
+                                     :include-rts false})
+    (get :body)
+    (first)
+    (get :id)))
 
 (defn get-tweet-ids-to-fav
   [handles]
@@ -37,22 +54,25 @@
       (let [[curr-handle & rem-handles] handles-to-iter
             last-id (get-last-id curr-handle)]
         (if (nil? last-id)
-          (recur rem-handles (conj tweet-ids-to-fav 
-                                   (get-first-tweet-id-from-timeline curr-handle)))
-          (recur rem-handles (conj tweet-ids-to-fav
-                                   (get-tweet-id-since last-id))))))))
+          (let [new-last-id (get-most-recent-status-id curr-handle)]
+            (swap! handle-last-id assoc curr-handle new-last-id)
+            (recur rem-handles (conj tweet-ids-to-fav 
+                                     new-last-id)))
+          (let [new-last-id (get-most-recent-status-id-since curr-handle
+                                                             last-id)]
+            (swap! handle-last-id assoc curr-handle new-last-id)
+            (recur rem-handles (conj tweet-ids-to-fav
+                                     new-last-id))))))))
+
+(defn put-favourite
+  [tweet-id]
+  (favorites-create :oauth-creds my-creds
+                    :params {:id tweet-id}))
 
 (defn -main
   []
-  (println (get-last-id "raghothams"))
-  (println (get-last-id "argvk"))
-  (println (get-tweet-ids-to-fav @handles)))
-  ;(println (lists-members :oauth-creds my-creds
-  ;            :params {:list_id "197805222"})))
-  ;(favorites-create :oauth-creds my-creds
-  ;                   :params {:id "572630868549758976"}))
-  ;(println (statuses-user-timeline :oauth-creds my-creds
-  ;                                 :params {:screen-name "raghothams"
-  ;                                          :exclude-replies true
-  ;                                          :count 1
-  ;                                          :include-rts false})))
+  ;(get-tweet-ids-to-fav @handles)
+  ;(println @handle-last-id))
+  (put-favourite "573208573976571905")
+  (put-favourite "572630868549758976")
+  (println "done"))
