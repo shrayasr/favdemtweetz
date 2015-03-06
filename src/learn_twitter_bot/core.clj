6dbe062b2a2cdc66510d3e314ff3dc59d2df03ca
Shrayas <shrayasr@gmail.com>
2015-03-06 23:55:59 +0530

    Basic scaffolding of project ready

--- src/learn_twitter_bot/core.clj
+++ src/learn_twitter_bot/core.clj
@@ -13,15 +13,44 @@
                                 "3063370765-T7TlE9bfO6SEaV6dvvGV6mXltOYRnLP6sBvVrNM"
                                 "EmSeWB17z8qyPBKwbWoNsrUM7TDQciYTJga3ehAHWY7ba"))
 
-(def peeps-to-stalk ["raghothams"
-                     "argvk"])
+(def handles (atom ["raghothams" "argvk"]))
+(def handle-last-id (atom {"argvk" 1}))
+
+(defn get-last-id 
+  [handle]
+  (get @handle-last-id handle))
+
+(defn get-first-tweet-id-from-timeline 
+  [handle]
+  1)
+
+(defn get-tweet-id-since
+  [last-id]
+  2)
+
+(defn get-tweet-ids-to-fav
+  [handles]
+  (loop [handles-to-iter handles
+         tweet-ids-to-fav []]
+    (if (empty? handles-to-iter)
+      tweet-ids-to-fav
+      (let [[curr-handle & rem-handles] handles-to-iter
+            last-id (get-last-id curr-handle)]
+        (if (nil? last-id)
+          (recur rem-handles (conj tweet-ids-to-fav 
+                                   (get-first-tweet-id-from-timeline curr-handle)))
+          (recur rem-handles (conj tweet-ids-to-fav
+                                   (get-tweet-id-since last-id))))))))
 
 (defn -main
   []
+  (println (get-last-id "raghothams"))
+  (println (get-last-id "argvk"))
+  (println (get-tweet-ids-to-fav @handles)))
   ;(println (lists-members :oauth-creds my-creds
   ;            :params {:list_id "197805222"})))
-  (favorites-create :oauth-creds my-creds
-                     :params {:id "572630868549758976"}))
+  ;(favorites-create :oauth-creds my-creds
+  ;                   :params {:id "572630868549758976"}))
   ;(println (statuses-user-timeline :oauth-creds my-creds
   ;                                 :params {:screen-name "raghothams"
   ;                                          :exclude-replies true
